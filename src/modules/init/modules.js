79c770abfa06692bbb973dcbe14fc78a973087e5
ozzyr <maxozzyr@gmail.com>
2018-07-02 21:44:29 -0300

    sunindo orcamento

--- src/modules/init/modules.js
+++ src/modules/init/modules.js
@@ -1,10 +1,13 @@
-import empresas from "../companies";
-import contratos from "../contratos";
-import vendedores from "../vendedores";
+import empresas from "../companies"
+import contratos from "../contratos"
+import vendedores from "../vendedores"
+import orcamentos from "../orcamentos"
+
 const modules = {
     empresas,
     contratos,
-    vendedores
+    vendedores,
+    orcamentos
 }
 
 export default modules
